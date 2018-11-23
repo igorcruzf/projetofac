10403085f5ee85706d775f23c88e9e78704293c2
Igor Cruz <42593480+igorcruzf@users.noreply.github.com>
2018-11-23 17:29:09 -0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from kivy.uix.image import Image
 import json
 
 import os
+
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -160,39 +161,40 @@ class Tarefas(Screen):
             #Imagem padrao
             img = cv.imread(foto_L,0)
             img2 = img.copy()
+            try:
+	        #Imagem a ser encontrada na imagem padrao
+                template = cv.imread(foto,0)
+                w, h = template.shape[::-1]
     
-            #Imagem a ser encontrada na imagem padrao
-            template = cv.imread(foto,0)
-            w, h = template.shape[::-1]
-    
-            # All the 6 methods for comparison in a list
-            #methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-                    #'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+                # All the 6 methods for comparison in a list
+                #methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+                        #'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
         
-            img = img2.copy()
-            method = eval('cv.TM_CCOEFF_NORMED')
+                img = img2.copy()
+                method = eval('cv.TM_CCOEFF_NORMED')
     
-	    # Apply template Matching
-            res = cv.matchTemplate(img,template,method)
-            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+	        # Apply template Matching
+                res = cv.matchTemplate(img,template,method)
+                min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
         
-            top_left = max_loc
+                top_left = max_loc
             
-            bottom_right = (top_left[0] + w, top_left[1] + h)
-            cv.rectangle(img,top_left, bottom_right, 255, 2)
+                bottom_right = (top_left[0] + w, top_left[1] + h)
+                cv.rectangle(img,top_left, bottom_right, 255, 2)
         
-        #plt.subplot(121),plt.imshow(template,cmap = 'gray')
-        #plt.title('Objeto da busca'), plt.xticks([]), plt.yticks([])
+            #plt.subplot(121),plt.imshow(template,cmap = 'gray')
+            #plt.title('Objeto da busca'), plt.xticks([]), plt.yticks([])
         
-            plt.plot(122),plt.imshow(img,cmap = 'gray')
-            plt.title('Ponto detectado'), plt.xticks([]), plt.yticks([])
-            plt.suptitle(x)
+                plt.plot(122),plt.imshow(img,cmap = 'gray')
+                plt.title('Ponto detectado'), plt.xticks([]), plt.yticks([])
+                plt.suptitle(x)
     
-        #para criar uma nova figura!
-            fig = plt.gcf() #getcurrentfigure
-            fig.savefig('Resposta.png')
-
-        #plt.show() #faz em formato de arquivo
+            #para criar uma nova figura!
+                fig = plt.gcf() #getcurrentfigure
+                fig.savefig('Resposta.png')
+            #plt.show() #faz em formato de executavel
+            except cv.error as e:
+                pass
         except AttributeError:
             pass
 
